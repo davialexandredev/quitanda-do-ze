8d8ef1e3d763b64968ccf094162fe94fc8debb6e
davialexandredev <105889845+davialexandredev@users.noreply.github.com>
2022-10-26 08:46:20 -0300

    ajustes finais

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+const menuItens = document.querySelectorAll('.nav-list a[href^="#"]');
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdClick);
+})
+
+function getScrollByHref(element){
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdClick(event){
+    event.preventDefault();
+    const to = getScrollByHref(event.target) - 80;
+
+    scrollToPosition(to);
+}
+
+function scrollToPosition(to){
+    window.scroll({
+        top: to,
+        behavior : "smooth",
+    });
+}
+
